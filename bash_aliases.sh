8ca390d2279bc207a267326463a55aff791ab5c9
DJ Schleen <djschleen@gmail.com>
2021-02-24 10:17:03 -0700

    Adds branch information when in a git repo

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# export PS1="\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;2m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\]:\[$(tput sgr0)\]\[\033[38;5;11m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\\$ \[$(tput sgr0)\]"
-export LSCOLORS=ExfxcxdxBxegedabagacad
-export CLICOLOR=1
+#region prompt
+
+function color_prompt {
+    local __user_and_host="\[\033[01;32m\]\u@\h"
+    local __cur_location="\[\033[01;34m\]\w"
+    local __git_branch_color="\[\033[31m\]"
+    local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
+    local __prompt_tail="\[\033[35m\]$"
+    local __last_color="\[\033[00m\]"
+    export PS1="$__user_and_host $__cur_location $__git_branch_color$__git_branch$__prompt_tail$__last_color "
+}
+color_prompt
+
+#endregion
 
 #region globals
 
